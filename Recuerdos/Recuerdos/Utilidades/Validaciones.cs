81dbf2c8fe8009441bfc619c861c2e493c3ea318
cubitosenpai <senpaicubito@gmail.com>
2019-10-30 19:17:02 -0500

    Se trabajo sobre las siguientes funcionalidades:
    1.Se agrega a la BD tabla "usuario" columna de "tipo" para el manejo de
    tipos de usuario.
    2.Crea vista de Registro junto con sus validaciones respectivas.
    3.Se crea en utilidades archivo con validaciones de c# y con regex (sin
    pruebas).
    
    nota: Aca no se valida nada de ingresos de usuario en ninguna ventana
    aun.
    
    fixes #8

---
+++ Recuerdos/Recuerdos/Utilidades/Validaciones.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recuerdos.Utilidades
+{
+    public class Validaciones
+    {
+        
+        public  void validarNumero(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar)&&!char.IsWhiteSpace(e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+        public void validarLetra(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+        public void validarLetrasNumerosEspacio(object sender, KeyPressEventArgs e)
+        {
+            //IsLetter(e.KeyChar) permite cualquier caracter entre la a y la z tanto mayusculas como minusculas
+            //'\b' permite poder eliminar caracteres con BackSpace
+
+            if (!char.IsLetter(e.KeyChar) && !char.IsNumber(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+        public void validarLetrasNumeros(object sender, KeyPressEventArgs e)
+        {
+            //IsLetter(e.KeyChar) permite cualquier caracter entre la a y la z tanto mayusculas como minusculas
+            //IsWhiteSpace permite espacio en blanco.
+            //'\b' permite poder eliminar caracteres con BackSpace
+
+            if (!char.IsLetter(e.KeyChar) && !char.IsNumber(e.KeyChar) && char.IsWhiteSpace(e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+
+        public Boolean formato_Numero(String dato)
+        {
+            string regex = @"^\d+$";
+            // Validación
+            if (Regex.IsMatch(dato, regex))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Valida que tenga formato de PIN osea 4 numeros
+        public Boolean formato_PIN(String dato)
+        {
+            string regex = @"^\d{4}$";
+            // Validación
+            if (Regex.IsMatch(dato, regex))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Valida que el dato tenga el formato de correo.
+        public Boolean formato_Correo(String dato)
+        {
+            string regex = @"^((([\w]+\.[\w]+)+)|([\w]+))@(([\w]+\.)+)([A-Za-z]{1,3})$";
+            // Validación
+            if (Regex.IsMatch(dato, regex))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Valida el formato de una contraceña con un rango de caracteres
+        public Boolean formato_Contracena(String dato)
+        {
+            string regex = @"^\w{6,8}$";
+            // Validación
+            if (Regex.IsMatch(dato, regex))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Valida el formato de una contraceña con un rango de caracteres
+        public Boolean formato_usuario(String dato)
+        {
+            string regex = @"/^[a-z0-9_-]{3,10}$/";
+            // Validación
+            if (Regex.IsMatch(dato, regex))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Valida el formato de un numero de telefono
+        public Boolean validar_Celular(String dato)
+        {
+            if (formato_Numero(dato) && dato.Length == 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Valida formato alfanumerico con espacios en un string yno  permite string en blanco
+        public Boolean formato_Cadena_Alfanumerica(String dato)
+        {
+            //string regex = @"[^a-zA-Z0-9]";
+            string regex = "/[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm";
+            // Validación
+            if (Regex.IsMatch(dato, regex))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Valida ingreso sea alfabetico y permite espacios
+        public Boolean formato_Alfabetico_Espacios(String dato)
+        {
+            string regex = @"[A-Za-z]$";
+            // Validación
+            if (Regex.IsMatch(dato, regex))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Valida formato alfanumerico en un palabra y no permite espacios en blanco
+        public Boolean formato_Palabra_Alfanumerica(String dato)
+        {
+            string regex = @"^[a-zA-Z0-9]+$";
+            // Validación
+            if (Regex.IsMatch(dato, regex))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Recuerdos/Recuerdos/Vista/Registro.cs
@@ -0,0 +1,108 @@
+﻿using Recuerdos.Utilidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recuerdos.Vista
+{
+    public partial class Registro : Form
+    {
+        //Instancias
+        static Validaciones objV = new Validaciones();
+        Conexion objCon = new Conexion();
+        SqlConnection con;
+        SqlDataReader consulta;
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        //Limitacion de escritura para el usuario en el campo de nombres
+        private void txtNombres_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            objV.validarLetrasNumerosEspacio(sender, e);
+        }
+
+        //Limitacion de escritura para el usuario en el campo de apellidos
+        private void txtApellidos_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            objV.validarLetrasNumerosEspacio(sender, e);
+        }
+
+        //Limitacion de escritura para el usuario en el campo de usuario
+        private void txtUsuario_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            objV.validarLetrasNumerosEspacio(sender, e);
+        }
+
+        //Limitacion de escritura para el usuario en el campo de contracena
+        private void txtContracena_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            objV.validarLetrasNumeros(sender, e);
+        }
+
+        //Funcionalidad de cancelar por medio un boton
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //funcionalidad de todo el proceso de registro por medio de boton
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if (txtNombres.Text != "" || txtApellidos.Text != "" || txtUsuario.Text != "" || txtContracena.Text != "" || txtCorreo.Text != "")
+            {
+                con=objCon.conectar();
+                consulta=objCon.consulta("select * from usuario where usuario='"+txtUsuario.Text+ "' or correo_electronico='"+txtCorreo.Text+"'", con);
+                if (consulta.Read())
+                {
+                    if (consulta["usuario"].ToString()==txtUsuario.Text)
+                    {
+                        MessageBox.Show("Lo sentimos ya existe un usuario con este nombre, intenta con otro.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lo sentimos ya existe un usuario con este correo electronico.");
+                    }
+                    objCon.cerrar(con);
+                }
+                else
+                {
+                    objCon.cerrar(con);
+                    con = objCon.conectar();
+                    int num = objCon.operar("Insert into usuario values('" + txtUsuario.Text + "','" + txtContracena.Text + "','" + txtNombres.Text + "','" + txtApellidos.Text + "','" + txtCorreo.Text + "');", con);
+                    objCon.cerrar(con);
+                    if (num > 0)
+                    {
+                        MessageBox.Show("Registro exitoso, ¡FELICIDADES! ☜(ﾟヮﾟ☜)");
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lo sentimos, algo a salido mal intentalo despues (┬┬﹏┬┬)");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Porfavor revisa los datos ingresados, uno de los campos esta vacio.");
+            }
+        }
+
+        //Limitacion de escritura para el usuario en el campo de correo electronico
+        private void txtCorreo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsSymbol(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
